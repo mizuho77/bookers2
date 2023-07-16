102db398fef6630a809b863f1e0889c5ef826ed4
mizuho77 <132583580+mizuho77@users.noreply.github.com>
2023-07-16 12:23:18 +0000

    [Finishi]4-27

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -26,7 +26,7 @@ class BooksController < ApplicationController
 
   def edit
     @book = Book.find(params[:id])
-    unless @book.id == current_user.id
+    unless @book.user == current_user
       redirect_to books_path
     end
   end
